bb89ed474a9cfdbf99c118a04880fad06ffc0a3a
mdennis876 <mdennis876@gmail.com>
2013-10-30 20:56:14 -0700

    Add Mashape xboxleader Api base URL

--- xboxleader/src/main/java/com/xboxleader/URL.java
+++ xboxleader/src/main/java/com/xboxleader/URL.java
@@ -5,17 +5,19 @@ package com.xboxleader;
  * @author mario
  */
 public enum URL {
+	MASHAPE ("https://xboxleaders.p.mashape.com/2.0/%s"),
 	BASE ("https://www.xboxleaders.com/api/2.0/%s");
 	private String url;
 	
 	URL (String url)
 	{
 		this.url = url;
-	}
+	}//end constructor
 	
 	@Override
 	public String toString() 
 	{
 		return url;
-	}
+	}//end toString method
+	
 }//end URL enum
